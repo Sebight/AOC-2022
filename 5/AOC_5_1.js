54665e8f327ce7c524847088e799c02c8f82c951
Seb <seb1rblx@gmail.com>
2022-12-05 08:57:34 +0100

    Day 5

---
+++ 5/AOC_5_1.js
@@ -0,0 +1,56 @@
+let input = ``;
+
+function solve() {
+
+    let stacks = [];
+    stacks.push([]); // 1
+    stacks.push([]); // 2
+    stacks.push([]); // 3
+    stacks.push([]); // 4
+    stacks.push([]); // 5
+    stacks.push([]); // 6
+    stacks.push([]); // 7
+    stacks.push([]); // 8
+    stacks.push([]); // 9
+
+    let lines = input.split('\n');
+    for (let line of lines) {
+        if (!line.includes("move")) {
+            let spaces = 0;
+            let doCheck = false;
+            for (let c of line) {
+                if (doCheck) {
+                    let index = (spaces - 1) / 4;
+                    console.log("Adding " + c + " to stack " + index);
+                    stacks[index].push(c);
+                    doCheck = false;
+                }
+
+                spaces++;
+                if (c === '[') {
+                    console.log("Found [ at " + spaces);
+                    doCheck = true;
+                    continue;
+                }
+            }
+        } else {
+            let amountOfCrates = line.split(' from ')[0].split(' ')[1];
+            let startingStack = line.split(' from ')[1].split(' to ')[0];
+            let endingStack = line.split(' to ')[1];
+
+            for (let i = 0; i < amountOfCrates; i++) {
+                let crateToMove = stacks[startingStack - 1][0];
+                stacks[startingStack - 1].splice(0, 1);
+                stacks[endingStack - 1].unshift(crateToMove);
+            }
+        }
+    }
+    console.log(stacks);
+    let string = "";
+    for (let stack in stacks) {
+        string += stacks[stack][0];
+    }
+    console.log(string);
+}
+
+solve();
